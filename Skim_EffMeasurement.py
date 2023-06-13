06c9f63d0cc4c6edfcca3de303a8a187dc368909
Elias Mettner <emettner@wisc.edu>
2023-06-13 14:23:12 -0500

    added comments

--- Skim_EffMeasurement.py
+++ Skim_EffMeasurement.py
@@ -7,26 +7,30 @@ import mplhep as hep
 from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 from coffea.nanoevents.methods import vector
 
+
+#read in file
 fname = "./NANO_NANO_3.root"
 events = NanoEventsFactory.from_root(
     fname,
     schemaclass=NanoAODSchema.v6,
 ).events()
 
+#define shortcut
 muons = events.Muon
 boostedTaus = events.boostedTau
 
+
+#mask cuts for all events
 muon_mask = (ak.num(muons) > 0) & ak.all(muons.pt > 30, axis=1) & ak.all(abs(muons.eta) < 2.5, axis=1) 
 boostedTau_mask = (ak.num(boostedTaus) > 0) & ak.all(boostedTaus.pt > 30, axis=1)
 
-mask = muon_mask & boostedTau_mask
 
-muEvents = events[muon_mask]
-boostedEvents = events[boostedTau_mask]
+#combine all cuts into one mask
+mask = muon_mask & boostedTau_mask
 
 selected_events = events[mask]
 
-
+#make 4-vectors
 muVec = ak.zip(
    {
     "pt": selected_events.Muon[:,0].pt,
@@ -48,10 +52,13 @@ boostedTauVec = ak.zip(
    with_name="PtEtaPhiMLorentzVector",
    behavior=vector.behavior,
 )
+
+#combine 4-vectors
 muTauVec = muVec.add(boostedTauVec)
 
 
 
+#plot and save
 fig, ax = plt.subplots()
 h = Hist(hist.axis.Regular(50,0,2,name="mass",label="GeV"))
 h.fill(muTauVec.mass)
