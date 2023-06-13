608698c9e2fe5389e5d2c91873fa3d36331c2556
Elias Mettner <emettner@wisc.edu>
2023-06-13 14:07:15 -0500

    first commit

---
+++ Skim_EffMeasurement.py
@@ -0,0 +1,61 @@
+import awkward as ak
+import uproot
+import hist
+from hist import Hist
+import matplotlib.pyplot as plt
+import mplhep as hep
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+from coffea.nanoevents.methods import vector
+
+fname = "./NANO_NANO_3.root"
+events = NanoEventsFactory.from_root(
+    fname,
+    schemaclass=NanoAODSchema.v6,
+).events()
+
+muons = events.Muon
+boostedTaus = events.boostedTau
+
+muon_mask = (ak.num(muons) > 0) & ak.all(muons.pt > 30, axis=1) & ak.all(abs(muons.eta) < 2.5, axis=1) 
+boostedTau_mask = (ak.num(boostedTaus) > 0) & ak.all(boostedTaus.pt > 30, axis=1)
+
+mask = muon_mask & boostedTau_mask
+
+muEvents = events[muon_mask]
+boostedEvents = events[boostedTau_mask]
+
+selected_events = events[mask]
+
+
+muVec = ak.zip(
+   {
+    "pt": selected_events.Muon[:,0].pt,
+    "eta": selected_events.Muon[:,0].eta,
+    "phi": selected_events.Muon[:,0].phi,
+    "mass": selected_events.Muon[:,0].mass,
+   },
+   with_name="PtEtaPhiMLorentzVector",
+   behavior=vector.behavior,
+)
+
+boostedTauVec = ak.zip(
+   {
+    "pt": selected_events.boostedTau[:,0].pt,
+    "eta": selected_events.boostedTau[:,0].eta,
+    "phi": selected_events.boostedTau[:,0].phi,
+    "mass": selected_events.boostedTau[:,0].mass,
+   },
+   with_name="PtEtaPhiMLorentzVector",
+   behavior=vector.behavior,
+)
+muTauVec = muVec.add(boostedTauVec)
+
+
+
+fig, ax = plt.subplots()
+h = Hist(hist.axis.Regular(50,0,2,name="mass",label="GeV"))
+h.fill(muTauVec.mass)
+hep.histplot(h)
+ax.set_title("Muon Vector + Boosted Tau Vector Mass")
+ax.set_xlabel("Mass (GeV)")
+fig.savefig("muTauVec_mass.png")
