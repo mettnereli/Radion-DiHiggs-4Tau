32c7da1772fc7f9dd2a30a365c056779bc17a387
Elias Mettner <emettner@wisc.edu>
2023-06-26 15:25:20 -0500

    swapped to DY sample

--- Skim_EffMeasurement.py
+++ Skim_EffMeasurement.py
@@ -1,6 +1,7 @@
 import sys
 import awkward as ak
 import uproot
+import boost_histogram as bh
 import hist
 from hist import Hist
 import matplotlib.pyplot as plt
@@ -29,11 +30,8 @@ class MyProcessor(processor.ProcessorABC):
                     & ak.all(muons.dz >= .0001, axis=1)
                     & ak.all(IsoLep1Value <= 30, axis=1))
       
-      boostedTau_mask =  (ak.all(boostedTaus.pt > 28, axis=1) 
-                         & ak.all(abs(boostedTaus.eta) <= 2.5, axis=1)
-                         & ak.all(boostedTaus.rawDeepTau2017v2p1VSmu > .3, axis=1)
-                         & ak.all(boostedTaus.rawDeepTau2017v2p1VSe > .3, axis=1) 
-                         & ak.all(boostedTaus.idDecayModeOldDMs >= 1, axis=1))
+      boostedTau_mask =  (ak.all(boostedTaus.pt > 20, axis=1) 
+                         & ak.all(abs(boostedTaus.eta) <= 2.5, axis=1))
       met_mask = events.MET.pt > 30
       dr = events.Muon[:,0].delta_r(events.boostedTau[:,0])
       dr_mask = ak.any(dr > .1, axis=0) & ak.any(dr < .8, axis=0)
@@ -42,23 +40,35 @@ class MyProcessor(processor.ProcessorABC):
       mask = muon_mask & boostedTau_mask & met_mask  & dr_mask
       selected_events = events[mask]
 
-      same_sign = ak.any((muons[:,0].charge == 1) & (boostedTaus[:,0].charge == 1), axis=0) & ak.any((muons[:,0].charge == -1) & (boostedTaus[:,0].charge == -1), axis=0)
-      opposite_sign = ak.any((muons[:,0].charge == 1) & (boostedTaus[:,0].charge == -1), axis=0) & ak.any((muons[:,0].charge == -1) & (boostedTaus[:,0].charge == 1), axis=0)
-      VVLooseNum = ak.all(selected_events.boostedTau.VVLooseIsolationMVArun2017v2DBoldDMwLT2017 > .5, axis =1)
+      signs = selected_events.Muon[:,0].charge * selected_events.boostedTau[:,0].charge
+      VVLooseNum = ak.all(selected_events.boostedTau.rawMVAnewDM2017v2 > .95, axis =1)
+      
+      print("Number of Leading Boosted Tau Before: ")
+      print(ak.num(ak.flatten(events.boostedTau.pt, axis=None), axis=0))
+
       print("Number of Leading Boosted Tau After: ")
-      print(ak.num(selected_events.boostedTau, axis=0))
+      print(ak.num(ak.flatten(selected_events.boostedTau.pt, axis=None), axis=0))
 
       histogram = Hist.new.Regular(10,0,500, name="pt", label ="$p_T (GeV)").StrCat(["opposite", "same"], name="sign", label = "Sign").StrCat(["denominator", "numerator"], name="fraction", label="Fraction").Double()
-      histogram.fill(sign="opposite", fraction="denominator", pt = ak.flatten(selected_events.boostedTau[opposite_sign].pt, axis=None))
-      histogram.fill(sign="opposite", fraction="numerator", pt = ak.flatten(selected_events.boostedTau[opposite_sign & VVLooseNum].pt, axis=None))
-      histogram.fill(sign="same", fraction="denominator", pt = ak.flatten(selected_events.boostedTau[same_sign].pt, axis=None))
-      histogram.fill(sign="same", fraction="numerator", pt = ak.flatten(selected_events.boostedTau[same_sign & VVLooseNum].pt, axis=None))
+      histogram.fill(sign="opposite", fraction="denominator", pt = ak.flatten(selected_events.boostedTau[signs == -1].pt, axis=None))
+      histogram.fill(sign="opposite", fraction="numerator", pt = ak.flatten(selected_events.boostedTau[signs == -1 & VVLooseNum].pt, axis=None))
+      histogram.fill(sign="same", fraction="denominator", pt = ak.flatten(selected_events.boostedTau[signs == 1].pt, axis=None))
+      histogram.fill(sign="same", fraction="numerator", pt = ak.flatten(selected_events.boostedTau[signs == 1 & VVLooseNum].pt, axis=None))
+
+      rate_os = (histogram[:,"opposite","denominator"]).view() / histogram[:,"opposite","numerator"].view()
+      rate_ss = (histogram[:,"same","denominator"]).view() / histogram[:,"same","numerator"].view()
+
+      fakeRate = Hist.new.Regular(10, 0, 500, name="fakerate", label="$p_T (GeV)").StrCat(["opposite", "same"], name="sign", label = "Sign").Double()
+      fakeRate.fill(sign="opposite", fakerate=rate_os)
+      fakeRate.fill(sign="same", fakerate=rate_ss)
+   
 
       return {
          dataset: {
             "entries": len(events),
             "events": selected_events,
-            "hist": histogram,
+            "pT": histogram,
+            "fakeRate": fakeRate
          }
       }
    
@@ -69,7 +79,7 @@ class MyProcessor(processor.ProcessorABC):
 if __name__ == "__main__":
    dataset = sys.argv[1]
    #read in file
-   fname = "./NANO_NANO_3.root"
+   fname = "./DY.root"
    events = NanoEventsFactory.from_root(
       fname,
       schemaclass=NanoAODSchema.v6,
@@ -83,10 +93,10 @@ if __name__ == "__main__":
    #plot and save
 
    fig, axs = plt.subplots(2, 2, figsize=(20, 20))
-   out[dataset]["hist"][:,"opposite","denominator"].plot1d(ax=axs[0, 0])
-   out[dataset]["hist"][:,"same","denominator"].plot1d(ax=axs[0, 1])
-   out[dataset]["hist"][:,"opposite","numerator"].plot1d(ax=axs[1, 0])
-   out[dataset]["hist"][:,"same","numerator"].plot1d(ax=axs[1, 1])
+   out[dataset]["pT"][:,"opposite","denominator"].plot1d(ax=axs[0, 0])
+   out[dataset]["pT"][:,"same","denominator"].plot1d(ax=axs[0, 1])
+   out[dataset]["pT"][:,"opposite","numerator"].plot1d(ax=axs[1, 0])
+   out[dataset]["pT"][:,"same","numerator"].plot1d(ax=axs[1, 1])
 
    axs[0, 0].set_title("OS_Denom_$p_T$")
    axs[0, 1].set_title("SS_Denom_$p_T$")
@@ -100,3 +110,16 @@ if __name__ == "__main__":
 
 
    fig.savefig("pT_Fake.png")
+
+   fig, ax = plt.subplots(figsize=(5, 10))
+   out[dataset]["fakeRate"][:,"opposite"].plot1d()
+   ax.set_title("OS $p_T$ Fake Rate")
+   ax.set_xlabel("$p_T$ (GeV)")
+   fig.savefig("pT_fakeRate_OS.png")
+
+
+   fig, ax = plt.subplots(figsize=(5, 10))
+   out[dataset]["fakeRate"][:,"same"].plot1d()
+   ax.set_title("SS $p_T$ Fake Rate")
+   ax.set_xlabel("$p_T$ (GeV)")
+   fig.savefig("pT_fakeRate_SS.png")
