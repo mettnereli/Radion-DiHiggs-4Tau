8e3bac4312bcdbfe8784d0a315d68063d7957f7a
Elias Mettner <emettner@wisc.edu>
2023-07-16 13:48:27 -0500

    fixed sign issues

--- Skim_EffMeasurement.py
+++ Skim_EffMeasurement.py
@@ -35,59 +35,62 @@ class MyProcessor(processor.ProcessorABC):
       events = events[(ak.num(events.Muon) > 0) & (ak.num(events.boostedTau) > 0)]
       muons = events.Muon
       boostedTaus = events.boostedTau
-      IsoLep1Value = muons.pfRelIso04_all/muons.pt
-     
       #mask cuts for all events
       muon_mask =  (ak.all(muons.pt > 28, axis=1) 
-                    & ak.all(abs(muons.eta) <= 2.5, axis=1) 
-                    & ak.all(muons.pfRelIso04_all < 1, axis=1)
-                    & ak.all(muons.dxy >= .0001, axis=1) 
-                    & ak.all(muons.dz >= .0001, axis=1)
-                    & ak.all(IsoLep1Value <= 30, axis=1))
+                    & ak.all(abs(muons.eta) < 2.4, axis=1) 
+                    & ak.all(muons.pfRelIso04_all < 1, axis=1))
       
       boostedTau_mask =  (ak.all(boostedTaus.pt > 20, axis=1) 
                          & ak.all(abs(boostedTaus.eta) <= 2.5, axis=1)
-                         & ak.all(boostedTaus.rawIsodR03 > .7, axis=1))
+                         & ak.all(boostedTaus.rawIsodR03 > .7, axis=1)
+                         & ak.all(boostedTaus.idAntiMu == 3, axis = 1))
       met_mask = events.MET.pt > 30
-      dr = events.Muon[:,0].delta_r(events.boostedTau[:,0])
-      dr_mask = ak.any(dr > .1, axis=0) & ak.any(dr < .8, axis=0)
       
       #combine all cuts into one mask
-      mask = muon_mask & boostedTau_mask & met_mask  & dr_mask
+      mask = muon_mask & boostedTau_mask & met_mask
       selected_events = events[mask]
 
-      signs = (selected_events.Muon[:,0].charge) * (selected_events.boostedTau[:,0].charge)
-      VVLooseNum = ak.all(selected_events.boostedTau.rawMVAnewDM2017v2 > .98, axis =1)
-      
-      print("Number of Leading Boosted Tau Before: ")
-      print(ak.num(ak.flatten(events.boostedTau.pt, axis=None), axis=0))
+      muon_boostedTau_pairs = ak.cartesian({'muon': selected_events.Muon, 'boostedTau': selected_events.boostedTau}, axis=1, nested=False)
 
-      print("Number of Leading Boosted Tau After: ")
-      print(ak.num(ak.flatten(selected_events.boostedTau.pt, axis=None), axis=0))
+      dr = muon_boostedTau_pairs['muon'].delta_r(muon_boostedTau_pairs['boostedTau'])
+      dr_mask = ak.all(dr > .1, axis=1) & ak.all(dr < .8, axis=1)
+      muon_boostedTau_pairs = muon_boostedTau_pairs[dr_mask]
 
-      print("Same sign: ", ak.num(ak.flatten(selected_events.boostedTau[signs == 1].pt, axis=None), axis=0))
-      print("Opposite sign: ", ak.num(ak.flatten(selected_events.boostedTau[signs == -1].pt, axis=None), axis=0))
+      signs = muon_boostedTau_pairs['muon'].charge * muon_boostedTau_pairs['boostedTau'].charge
 
-      muonVec = self.makeVector(selected_events.Muon[:,0])
-      tauVec = self.makeVector(selected_events.boostedTau[:,0])
+      OS_pairs = muon_boostedTau_pairs[ak.all(signs == -1, axis=1)]
+      SS_pairs = muon_boostedTau_pairs[ak.all(signs == 1, axis=1)]
+      print("OS: ", ak.num(OS_pairs, axis=0))
+      print("SS: ", ak.num(SS_pairs, axis=0))
+
+      OS_VVLooseNum = ak.all(OS_pairs['boostedTau'].rawMVAnewDM2017v2 > .5, axis =1)
+      SS_VVLooseNum = ak.all(SS_pairs['boostedTau'].rawMVAnewDM2017v2 > .5, axis =1)
+
+      muonVec = self.makeVector(ak.flatten(muon_boostedTau_pairs['muon'], axis=1))
+      tauVec = self.makeVector(ak.flatten(muon_boostedTau_pairs['boostedTau'], axis=1))
       muTauVec = muonVec.add(tauVec)
 
-      vec_pt = Hist.new.Regular(100,300,800, name='vec_pt', label="$p_T$ (GeV)").Double()
+      vec_pt = Hist.new.Regular(100,0,600, name='vec_pt', label="$p_T$ (GeV)").Double()
       vec_pt.fill(vec_pt = muTauVec.pt)
 
       vec_mass = Hist.new.Regular(100,0,400, name='vec_mass', label="Mass(GeV)").Double()
       vec_mass.fill(vec_mass = muTauVec.mass)
 
+      OS_flat_pt_denom = ak.flatten(OS_pairs['boostedTau'].pt, axis = None)
+      OS_flat_pt_num = ak.flatten(OS_pairs['boostedTau'][OS_VVLooseNum].pt, axis = None)
+      SS_flat_pt_denom = ak.flatten(SS_pairs['boostedTau'].pt, axis = None)
+      SS_flat_pt_num = ak.flatten(SS_pairs['boostedTau'][SS_VVLooseNum].pt, axis = None)
+
       boosted_pt = Hist.new.Regular(10,0,500, name="pt", label ="$p_T$ (GeV)").StrCat(["opposite", "same"], name="sign", label = "Sign").StrCat(["denominator", "numerator"], name="fraction", label="Fraction").Double()
-      boosted_pt.fill(sign="opposite", fraction="denominator", pt = ak.flatten(selected_events.boostedTau[signs == -1][:,0].pt, axis=None))
-      boosted_pt.fill(sign="opposite", fraction="numerator", pt = ak.flatten(selected_events.boostedTau[signs == -1 & VVLooseNum][:,0].pt, axis=None))
-      boosted_pt.fill(sign="same", fraction="denominator", pt = ak.flatten(selected_events.boostedTau[signs == 1][:,0].pt, axis=None))
-      boosted_pt.fill(sign="same", fraction="numerator", pt = ak.flatten(selected_events.boostedTau[signs == 1 & VVLooseNum][:,0].pt, axis=None))
+      boosted_pt.fill(sign="opposite", fraction="denominator", pt = OS_flat_pt_denom)
+      boosted_pt.fill(sign="opposite", fraction="numerator", pt = OS_flat_pt_num)
+      boosted_pt.fill(sign="same", fraction="denominator", pt = SS_flat_pt_denom)
+      boosted_pt.fill(sign="same", fraction="numerator", pt = SS_flat_pt_num)
 
-      boosted_pt_rate_os = boosted_pt[:,"opposite","numerator"].view() / (boosted_pt[:,"opposite","denominator"]).view()
-      boosted_pt_rate_ss = boosted_pt[:,"same","numerator"].view() / (boosted_pt[:,"same","denominator"]).view()
+      boosted_pt_rate_os = boosted_pt[:,"opposite","numerator"] / (boosted_pt[:,"opposite","denominator"])
+      boosted_pt_rate_ss = boosted_pt[:,"same","numerator"] / (boosted_pt[:,"same","denominator"])
 
-      boosted_pt_fakeRate = Hist.new.Regular(10, 0, 500, name="fakerate", label="$p_T (GeV)").StrCat(["opposite", "same"], name="sign", label = "Sign").Double()
+      boosted_pt_fakeRate = Hist.new.Regular(50, 0, 500, name="fakerate", label="$p_T (GeV)").StrCat(["opposite", "same"], name="sign", label = "Sign").Double()
       boosted_pt_fakeRate.fill(sign="opposite", fakerate=boosted_pt_rate_os)
       boosted_pt_fakeRate.fill(sign="same", fakerate=boosted_pt_rate_ss)
    
@@ -97,7 +100,8 @@ class MyProcessor(processor.ProcessorABC):
             "entries": len(events),
             "events": selected_events,
             "pT": boosted_pt,
-            "fakeRate": boosted_pt_fakeRate,
+            "OS_fakeRate": boosted_pt_rate_os,
+            "SS_fakeRate": boosted_pt_rate_ss,
             "vec_pt": vec_pt,
             "vec_mass": vec_mass,
          }
@@ -144,27 +148,26 @@ if __name__ == "__main__":
    fig.savefig("./plots/pT_Fake.png")
 
    fig, ax = plt.subplots(figsize=(10,10))
-   out[dataset]["fakeRate"][:,"opposite"].plot1d()
+   out[dataset]["OS_fakeRate"].plot1d()
    ax.set_title("OS $p_T$ Fake Rate")
    ax.set_xlabel("$p_T$ (GeV)")
    fig.savefig("./plots/pT_fakeRate_OS.png")
 
-
    fig, ax = plt.subplots(figsize=(10, 10))
-   out[dataset]["fakeRate"][:,"same"].plot1d()
+   out[dataset]["SS_fakeRate"].plot1d()
    ax.set_title("SS $p_T$ Fake Rate")
    ax.set_xlabel("$p_T$ (GeV)")
    fig.savefig("./plots/pT_fakeRate_SS.png")
 
    fig, ax = plt.subplots(figsize=(10, 10))
    out[dataset]["vec_pt"].plot1d()
-   ax.set_title("Muon + Bosoted Tau Vector $p_T$")
+   ax.set_title("Muon + Boosted Tau Vector $p_T$")
    ax.set_xlabel("$p_T$ (GeV)")
    fig.savefig("./plots/muTau_vec_pt.png")
 
    fig, ax = plt.subplots(figsize=(10, 10))
    out[dataset]["vec_mass"].plot1d()
-   ax.set_title("Muon + Bosoted Tau Vector Mass")
+   ax.set_title("Muon + Boosted Tau Vector Mass")
    ax.set_xlabel("Mass (GeV)")
    fig.savefig("./plots/muTau_vec_mass.png")
 
